7620948ab235281fa2d03242dd9313be8c5ff5f8
Sergei Tikhomirov <s.tikhomirov@arenadata.io>
2025-10-02 02:34:42 +0300

    Extended the monitoring ability to multiple threads

--- src/message_handler.py
+++ src/message_handler.py
@@ -22,7 +22,7 @@ file_downloader = FileDownloader()
 
 @message_router.message()
 async def analyze_message(message: types.Message):
-    if message.message_thread_id != 2: # Monitoring only specified topic (thread)
+    if message.message_thread_id not in config.MONITORED_THREADS: # Monitoring only specified topics (threads)
         return
     list_of_urls = []
     article_urls = message_parser.parse(message.text)
